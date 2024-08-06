1d9d4f36b1aed17b8bc914688cae68b62b2c0b29
ChiefMatias <sergio.tiago.matias@gmail.com>
2024-08-06 08:32:17 +0200

    updated test

--- python/test.py
+++ python/test.py
@@ -1,12 +1,13 @@
 import socket
+import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('localhost', 6379))
 
-sock.sendall(b'$5\r\Hello\r\n')
+sock.sendall(b'$5\r\nHello\r\n')
 
 #response = sock.recv(1024)
 #print(response.decode('utf-8'))
 
 sock.close()
-exit()
+sys.exit(0)
